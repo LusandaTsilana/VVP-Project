f6e1ff5570afbdd40a467e1398d39bcc8b08c2b4
Lusanda Tsilana <124098032+LusandaTsilana@users.noreply.github.com>
2023-08-15 15:44:50 +0200

    ignore

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
     
-    <nav class="md:bg-customColor shadow">
+    <nav class="md:bg-customColor shadow ">
         <div class = "sticky text-black-300  md:flex md:w-4/6 justify-between py-3.5 px-12 
             items-center ">
     
             <div class = "flex items-center cursor-pointer" href = "#">
                 <span class="mr-1">
-                    <img src="/src/assets/images/lusanda (1).png" alt="logo" width="83.08" height="80">
+                    <img class="w-24 md:w-24" src="/src/assets/images/lusanda (1).png" alt="logo">
                 </span>
 
             </div>
@@ -17,12 +17,13 @@
 
             <ul class = "font-semibold md:flex md:items-center gap-[1vw] md:px-0 px-10 md:pb-0 pb-2 md:static absolute bg-customColor sm:shadow md:shadow-none w-full md:w-auto  duration-500 ease-in md:mt-0 mt-4" :class="[open ? 'right-0' :'right-[-100%]']">
             <li class="md:mx-4 md:my-0 my-8 no-underline hover:underline" v-for = "link in Links">
-                <a :href = "link.link" class="text-lg md:text-xl" >{{ link.name }}</a>
+                <a :href = "link.link" class="text-lg md:text-2xl" >{{ link.name }}</a>
             </li>
             
             </ul>
     </div>
 </nav>
+
 
 </template>
 
