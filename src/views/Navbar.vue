ac10ddf1a4cb333e06338614fdd2640fe258c6a5
Lusanda Tsilana <sanda.tsilana@gmail.com>
2023-08-15 22:51:32 +0200

    ignored

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -4,7 +4,7 @@
         <div class = "text-black-300 w-3/4 md:flex justify-between py-3.5 px-10 
             items-center ">
     
-            <div class = "flex items-center cursor-pointer" href = "#">
+            <div class = "flex items-center cursor-pointer" href = "Home.vue">
                 <span class="mr-1">
                     <img class="w-24 md:w-24" src="/src/assets/images/lusanda (1).png" alt="logo">
                 </span>
@@ -19,7 +19,7 @@
 
             <ul class = "font-semibold md:flex md:items-center md:px-0 px-10 md:pb-0 pb-10 md:static absolute bg-customColor shadow md:shadow-none w-full md:w-auto top-35 duration-500 ease-in md:mt-0 mt-4" :class="[open ? 'right-0' :'right-[-100%]']">
             <li class="md:mx-4 md:my-0 my-8 no-underline hover:underline" v-for = "link in Links">
-                <a :href = "link.link" class="text-xl" >{{ link.name }}</a>
+                <a :href = "link.link" class="text-xl xl:2xl" >{{ link.name }}</a>
             </li>
             
             </ul>
