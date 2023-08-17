b34e43f5a06a20251f3f5c301053d22271dc75de
Lusanda Tsilana <sanda.tsilana@gmail.com>
2023-08-17 15:21:21 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <script setup>
 import Navbar from "@/views/Navbar.vue";
 import FooterBand from "@/views/FooterBand.vue";
+
 </script>
 
 <template>
     <body class = "bg-white">
 
-        <header class="">
             <div id="app">
                 <Navbar/>
-            </div>
-
-        </header>
-
-        <main class="bg-white flex-grow px-4 py-32" >
-            <div>
-                <Home/>
-            </div>
-        </main>
-            
-        <footer class="py-4">
-            <div>
+                <router-view />
                 <FooterBand/>
             </div>
-        </footer>
-
     </body>
     
 
